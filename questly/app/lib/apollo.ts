223665f6143435734d2f9f8977e5e3f553ef323a
Paweł Romik <leoneczek15@gmail.com>
2026-08-11 01:20:23 +0200

    chore: use CMS URL from environment

--- questly/app/lib/apollo.ts
+++ questly/app/lib/apollo.ts
@@ -2,6 +2,6 @@ import { HttpLink } from "@apollo/client";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-	link: new HttpLink({ uri: "http://localhost:1337/graphql" }),
+	link: new HttpLink({ uri: process.env.NEXT_PUBLIC_CMS_URL }),
 	cache: new InMemoryCache()
 });
